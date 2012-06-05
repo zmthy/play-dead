ad1466729602e23a84b1c18279e8f23da0d02115
Daniel Atkins <paradoxical.prophet@gmail.com>
2012-06-06 01:45:55 +1200

    Added PlatformTest folder. This is a VS2010 project that contains the very very basics of basics for a platforming engine. I will be refactoring this and expanding its capabilities over the next couple of days---this commit is so you guys can start looking at how it interacts with VS and XNA and stuff

---
+++ PlatformTest/PlatformTest/PlatformTest/Character.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace PlatformTest
+{
+    class Character
+    {
+        Rectangle bounds = new Rectangle(57, 337, 40, 48);
+        Rectangle srcBounds = new Rectangle(0, 0, 40, 48); //Determines which frame is showing
+
+        string direction = "stand";
+        string facing = "right";
+
+        int frameCount = 0; //Counts which frame is currently in sourceBounds
+        const int delay = 4;
+
+        float speed = 0f;
+        const float maxspeed = 12f;
+
+        int startY = 0;
+        int jumpCount = 0;
+
+        public void KeyInput()
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            if (direction != "up")
+            {
+                if (keyState.IsKeyDown(Keys.D))
+                {
+                    direction = "right";
+                    facing = "right";
+                }
+                if (keyState.IsKeyDown(Keys.A))
+                {
+                    direction = "left";
+                    facing = "left";
+                }
+                if (keyState.IsKeyDown(Keys.W))
+                {
+                    direction = "up";
+                }
+                if (keyState.IsKeyUp(Keys.D) && direction == "right")
+                {
+                    direction = "stand";
+                    facing = "right";
+                }
+                if (keyState.IsKeyUp(Keys.A) && direction == "left")
+                {
+                    direction = "stand";
+                    facing = "left";
+                }
+            }
+            else
+            {
+                if (keyState.IsKeyDown(Keys.D))
+                {
+                    if (speed < maxspeed)
+                        speed += .2f;
+                    facing = "right";
+                }
+                if (keyState.IsKeyDown(Keys.A))
+                {
+                    if (speed > -maxspeed)
+                        speed -= .2f;
+                    facing = "left";
+                }
+            }
+        }
+
+        bool debug = false;
+        public void Move()
+        {
+            if (direction != "up" && !CheckCollision(bounds) && debug)
+            {
+                //fall
+                startY = bounds.Y + 99;
+                direction = "up";
+                jumpCount = 11;
+            }
+
+            if (frameCount % delay == 0)
+            {
+                switch (direction)
+                {
+                    case "stand":
+                        Equalize(2);
+                        bounds.X += (int)speed;
+                        if (frameCount / delay >= 4)
+                            frameCount = 0;
+                        srcBounds = new Rectangle(frameCount / delay * 40, 0, 40, 48);
+                        break;
+                    case "left":
+                        if (speed > -maxspeed)
+                            speed -= 1;
+                        bounds.X += (int)speed;
+                        if (frameCount / delay >= 8)
+                            frameCount = 0;
+                        srcBounds = new Rectangle(frameCount / delay * 40, 48, 40, 48);
+                        break;
+                    case "right":
+                        if (speed < maxspeed)
+                            speed += 1;
+                        bounds.X += (int)speed;
+                        if (frameCount / delay >= 8)
+                            frameCount = 0;
+                        srcBounds = new Rectangle(frameCount / delay * 40, 48, 40, 48);
+                        break;
+                    case "up":
+                        debug = true;
+                        if (speed > -4 && speed < 4)
+                            srcBounds.Y = 96;
+                        else
+                            srcBounds.Y = 48;
+                        if (srcBounds.Y == 0 || srcBounds.Y == 96)
+                        {
+                            if (jumpCount < 2)
+                            {
+                                if (frameCount / delay >= 9)
+                                    frameCount = 0;
+                            }
+                            else if (jumpCount > 2 && jumpCount <= 10)
+                            {
+                                if (frameCount / delay > 3)
+                                    frameCount = 2 * delay;
+                            }
+                            else if (jumpCount > 10 && jumpCount <= 18)
+                            {
+                                if (frameCount / delay > 5)
+                                    frameCount = 4 * delay;
+                            }
+                            else if (jumpCount > 18 && CheckCollision(new Rectangle(bounds.X, bounds.Y + Map.size, bounds.Width, bounds.Height)))
+                            {
+                                if (frameCount / delay >= 9)
+                                    frameCount = 0;
+                            }
+                            else if (jumpCount > 18) //if he's falling a long time, don't loop the animation where he hits the ground
+                                frameCount = 4 * delay;
+
+                            srcBounds = new Rectangle(frameCount / delay * 40, 96, 40, 48);
+                        }
+                        else if (srcBounds.Y == 48)
+                        {
+                            if (frameCount / delay >= 8)
+                                frameCount = 0;
+                            if (jumpCount <= 10)
+                                srcBounds = new Rectangle((frameCount / delay) / 2 * 40, 48, 40, 48);
+                            else
+                                srcBounds = new Rectangle(frameCount / delay * 40, 48, 40, 48);
+                        }
+                        if (jumpCount == 0)
+                            startY = bounds.Y;
+
+                        //Sets function for determining jump y-position
+                        float jump_positionY = (jumpCount - 10) * (jumpCount - 10) - 100 + startY;
+                        //Sets terminal velocity
+                        float max_velocityY = 2 * (20 - 10); //derivative of jump_positionY where 20 is max jumpCount value
+                        float max_positionY = startY + max_velocityY * (jumpCount - 20);
+
+                        if (jumpCount >= 20)
+                            jump_positionY = max_positionY;
+
+                        bounds = new Rectangle(bounds.X + (int)speed,
+                            (int)jump_positionY, 40, 48);
+                        jumpCount++;
+                        break;
+                }
+            }
+
+            //Check collision
+            if (jumpCount >= 10 && CheckCollision(bounds))
+            {
+                //Stop
+                direction = "stand";
+                jumpCount = 0;
+            }
+
+            frameCount++;
+        }
+
+        //Checks collision
+        private bool CheckCollision(Rectangle bounds)
+        {
+            int cutoff = bounds.Y + Map.size;
+
+            //Create a rectangle for the current block, if it is not -1
+            Rectangle tempBounds = new Rectangle(bounds.X, cutoff, bounds.Width, cutoff - bounds.Y);
+            tempBounds.Y += tempBounds.Height - Map.size;
+
+            int x = (tempBounds.X + bounds.Width / 2) / Map.size;
+            int y = tempBounds.Y / Map.size + 1;
+            int type = Map.getType(y, x);
+
+            if (type > -1)
+            {
+                //Test to see if the character collides
+                Rectangle tileBounds = new Rectangle(x * Map.size, y * Map.size, Map.size, Map.size);
+                if (tempBounds.Intersects(tileBounds))
+                {
+                    this.bounds = new Rectangle(bounds.X, tileBounds.Y - bounds.Height, bounds.Width, bounds.Height);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Equalize(int i)
+        {
+            for (int k = 0; k < i; k++)
+            {
+                if (speed < 0f)
+                    speed += 1;
+                else if (speed > 0f)
+                    speed -= 1;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D texture)
+        {
+            if (facing == "right")
+                spriteBatch.Draw(texture, bounds, srcBounds, Color.White);
+            else
+                spriteBatch.Draw(texture, bounds, srcBounds, Color.White,
+                    0f, new Vector2(0, 0), SpriteEffects.FlipHorizontally, 0f);
+        }
+    }
+}
---
+++ PlatformTest/PlatformTest/PlatformTest/Map.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace PlatformTest
+{
+    class Map
+    {
+        public const int size = 32; //The size of each tile in pixels
+        public static int height; //in tiles
+        public static int width; //in tiles
+
+        static int[,] tileMap;
+
+        public Map(GraphicsDevice graphics) //Creates a new map based on the size of the screen
+        {
+            height = graphics.DisplayMode.Height / size;
+            width = graphics.DisplayMode.Width / size;
+
+            tileMap = new int[height, width]; //Initializes the new map
+
+            for (int y = 0; y < height; y++)  //Defaults all values to -1 (saves processing time)
+                for (int x = 0; x < width; x++)
+                    tileMap[y, x] = -1;
+            for (int x = 0; x < width; x++) //Better put a floor down!
+                tileMap[12, x] = 1;
+            for (int y = 13; y < height; y++)  //Fill everything under the floor so it looks nice
+                for (int x = 0; x < width; x++)
+                    tileMap[y, x] = 4;
+            
+        }
+
+        public static int getType(int y, int x)
+        {
+            if (!(x < 0 || y < 0) && !(x > width || y > height))
+            {
+                return tileMap[y, x];
+            }
+            else
+                return -1;
+        }
+
+        public void Set(int y, int x, int currentTile) //SAFELY sets the value of a clicked tile based on the currently selected tile in the editor
+        {
+            if (!(x < 0 || y < 0))
+            {
+                tileMap[y, x] = currentTile;
+            }
+        }
+
+        public void DrawMap(SpriteBatch spriteBatch, Texture2D tileSheet)
+        {
+            height = tileMap.GetLength(0); //Map height (in tiles)
+            width = tileMap.GetLength(1); //Map width (in tiles)
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                {
+                    int cell = tileMap[y, x]; //Cell = value of tile at map position (y,x)
+
+                    if (cell > -1)//If cell exists; otherwise, continue (saves processing time)
+                    {
+                        Rectangle sourceBounds = SourcePoint(cell, tileSheet); //Gets coodinates of tile on the tilesheet
+                        Rectangle bounds = new Rectangle(x * size, y * size, size, size); //Sets where and what size the tile will be in the game
+
+                        spriteBatch.Draw(tileSheet, bounds, sourceBounds, Color.White); //Draws the tile
+                    }
+                }
+        }
+
+        private Rectangle SourcePoint(int cell, Texture2D tileSheet) //Takes an integer value and converts it to its corresponding tile location
+        {
+            //Example of the data processed:
+            //0  1  2
+            //3  4  5
+            //6  7  8
+            //9  10 11
+            //12 13 14
+            //15 16 17
+            //
+            //If cell = 7, for example:
+            //tileWidth = 3
+            //y = 7 / 3 = 2
+            //x = 7 % 3 (the remainder of 7 / 3) = 1
+            //(x,y) value returned = (32,64)
+
+            Rectangle point = new Rectangle(0, 0, size, size); //The point to be returned
+            int tileWidth = tileSheet.Width / size;              //How many tiles can fit in the tileset horizontally
+            int y = cell / tileWidth;                         //Returns the row of the tile
+            int x = cell % tileWidth;                         //Returns how many tiles "cell" is displaced by
+
+            point = new Rectangle(x * size, y * size, size, size); //Compacts the above data into a source rectangle
+
+            return point; //returns value
+        }
+
+    }
+}
